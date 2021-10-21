4a706c7ae15a20454342769e0695d8e1fa5121af
Team LevX <team@levx.io>
2021-10-22 06:35:49 +0900

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import "./styles/App.css";
 import NFTAirdrop from "./components/NFTAirdrop";
 import ShoyuLogo from "./components/ShoyuLogo";
 import useEthereum from "./hooks/useEthereum";
+import LogoSquared from "./images/logo-squared.jpeg";
 import sak3 from "./data/sak3.json";
 
 function App() {
@@ -27,13 +28,38 @@ function App() {
             )}
           </div>
         </header>
-        <div>
+        <div className={"body"}>
           <Switch>
             <Route path="/">
               <NFTAirdrop data={sak3} context={context} />
             </Route>
           </Switch>
         </div>
+        <div className={"border"} />
+        <footer>
+          <img src={LogoSquared} alt={"logo"} width={96} height={96} />
+          <div className={"social"}>
+            <a href={"https://twitter.com/SHOYU_NFT"} target={"_blank"}>
+              Twitter
+            </a>
+            <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+            <a href={"https://discord.gg/rKuc868Fv3"} target={"_blank"}>
+              Discord
+            </a>
+          </div>
+          <div className={"copyright"}>
+            Powered by{" "}
+            <a href={"https://sushi.com"} target={"_blank"}>
+              SushiSwap
+            </a>
+          </div>
+          <div className={"copyright"}>
+            Built by{" "}
+            <a href={"https://twitter.com/LevxApp"} target={"_blank"}>
+              LevX
+            </a>
+          </div>
+        </footer>
       </div>
     </Router>
   );
